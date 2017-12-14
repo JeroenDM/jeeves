a692c3bb6f24a9442ea98aae9a2eba68fde6db8d
jeroendm <jeroendemaeyer@live.be>
2017-12-14 16:01:58 +0100

    work on code

--- data_logger.py
+++ data_logger.py
@@ -16,7 +16,7 @@ import os.path
 # TODO caching to avoid to many file operations?
 
 class DataLogger:
-    def __init__(self, path, filename, header):
+    def __init__(self, path, filename, header, log_interval=9):
         # setup and check file path and name
         self.p = path + filename
         if os.path.exists(self.p):
@@ -28,15 +28,48 @@ class DataLogger:
         # write initial header
         df = pd.DataFrame(columns=header)
         df.to_csv(self.p, index=False)
+        
+        # initialize cache
+        self._reset_cache()
+        
+        # keep last logging time
+        self.i = 0
+        self.di_max = log_interval
     
     def log(self, *arg, **kwarg):
+        if self.i >= self.di_max:
+            self.i = 0
+            self._append_to_cache(*arg, **kwarg)
+            self._write_to_file()
+            self._reset_cache()
+        else:
+            self.i += 1
+            self._append_to_cache(*arg, **kwarg)
+    
+    def flush_cache(self):
+        self._write_to_file()
+        self._reset_cache()     
+    
+    def _write_to_file(self):
+        self.cache.to_csv(self.p, columns=self.h, index=False, mode='a', header=False)
+        self._reset_cache()
+    
+    def _append_to_cache(self, *arg, **kwarg):
         if not len(kwarg) == self.n_col:
             raise ValueError("Wrong number of variables")
         data = {}
         for key in self.h:
             data[key] = kwarg[key]
-        df = pd.DataFrame(data)
-        df.to_csv(self.p, columns=self.h, index=False, mode='a', header=False)
+        try:
+            df = pd.DataFrame(data)
+        except ValueError:
+            # solution for error
+            # ValueError: If using all scalar values, you must pass an index
+            df = pd.DataFrame(data, index=[0])
+        self.cache = self.cache.append(df, ignore_index=True)
+    
+    def _reset_cache(self):
+        self.cache = pd.DataFrame(columns=self.h)
 
 if __name__ == "__main__":
     print("------------------------")
@@ -51,7 +84,9 @@ if __name__ == "__main__":
         bi = np.random.rand(5)
         dl.log(a=ai, b=bi)
         dl.log(a=ai, b=bi)
+    dl.flush_cache()
     end = time.time()
-    print(end - start)
+    #print(end - start)
     res = pd.read_csv('data.csv')
+    print("Data from csv file (first rows):")
     print(res.head())
--- prime_numbers.py
+++ prime_numbers.py
@@ -6,12 +6,27 @@ Created on Thu Dec 14 14:46:35 2017
 @author: jeroen
 """
 
-#from data_logger import DataLogger
-#
-#var_names = ['i', 'pn']
-#path = '/home/jeroen/Documents/gitlab/jeeves/'
-#name = 'prime_numbers_1.csv'
-#logger = DataLogger(path, name, var_names)
+import sys
+import os
+from data_logger import DataLogger
+
+# process command line input
+if len(sys.argv) > 1:
+    N_max = int(sys.argv[1])
+    if len(sys.argv) > 2:
+        name = sys.argv[2]
+    else:
+        name = 'prime_numbers_1.csv'
+else:
+    print("Using default value N_max = 100")
+    print("Optional command line argument to change this")
+    N_max = 100
+    name = 'prime_numbers_1.csv'
+
+# setup other default parameters
+var_names = ['i', 'pn']
+path = os.getcwd() + '/'
+logger = DataLogger(path, name, var_names, log_interval=99)
 
 def is_prime(n):
     for ni in range(2, n):
@@ -19,8 +34,10 @@ def is_prime(n):
             return False
     return True
 
-N_max = 100
-
+index = 0
 for i in range(N_max):
     if is_prime(i):
-        print(i)
+        print(index, i)
+        logger.log(i=index, pn=i)
+        index += 1
+logger.flush_cache()
