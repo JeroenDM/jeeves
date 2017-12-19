fc48342cb5d46e78e45c8d4e09f1cf5ad8729101
jeroendm@pi <jeroendemaeyer@live.be>
2017-12-19 20:22:12 +0000

    auto commit finished signal

--- finished.bash
+++ finished.bash
@@ -1,4 +1,5 @@
 echo "I'm done!" >> readme.md
+date >> readme.md
 git add .
 git commit -m "auto commit finished signal"
 git push
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 I'm done!
 I'm done!
 I'm done!
+I'm done!
+Tue Dec 19 20:22:12 UTC 2017
